08907e4cf08b19d07a9d2adba635196f8d28495c
Ritesh Shinde <114718220+riteshshinde04@users.noreply.github.com>
2024-03-20 22:05:12 +0530

    Add files via upload

---
+++ Solution 12.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    //Function to check whether a Binary Tree is BST or not.
+    boolean isBST(Node root)
+    {
+        // code here.
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    boolean isBSTUtil(Node node, int min, int max)
+    {
+        if (node == null)
+            return true;
+        
+        if (node.data < min || node.data > max)
+            return false;
+        
+        return (isBSTUtil(node.left, min, node.data-1) &&
+                isBSTUtil(node.right, node.data+1, max));
+    }
+}
