b8e1426cc333d835750f9307a3685357d26ed9da
Ritesh Shinde <114718220+riteshshinde04@users.noreply.github.com>
2024-04-05 19:55:42 +0530

    Add files via upload

---
+++ Solution 28.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here 
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        int i=0, j=0, k=0;
+        
+        while(i<n1 && j<n2 && k<n3){
+            if(A[i] == B[j] && A[i] == C[k]){
+                if(!arr.contains(A[i]))
+                    arr.add(A[i]);
+                    i++; j++; k++;
+            }else if(A[i] < B[j]){
+                i++;
+            }else if(B[j] < C[k]){
+                j++;
+            }else{
+                k++;
+            }
+        }
+        
+        return arr;
+    }
+}
