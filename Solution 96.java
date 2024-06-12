f6f250f926a28b85641c094e541f23240bc74872
Ritesh Shinde <114718220+riteshshinde04@users.noreply.github.com>
2024-06-12 12:37:21 +0530

    Add files via upload

---
+++ Solution 96.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int countNumberswith4(int n) {
+        // code here
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i+"").contains("4")) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
